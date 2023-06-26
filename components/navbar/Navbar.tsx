07b4ec30006851b8cfb8caced8358cee40eaa377
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-26 08:52:13 +0000

    UI for Navbar done

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 export default function Navbar() {
     return <div className="fixed w-full bg-white z-10 shadow-sm">
@@ -9,6 +13,8 @@ export default function Navbar() {
                     className="flex flex-row items-center justify-between gap-3 md:gap-0"
                 >
                     <Logo />
+                    <Search />
+                    <UserMenu />
                 </div>
             </Container>
         </div>
